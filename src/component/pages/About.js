c1ac08908b67e86b91c38df466f709486c9ba53c
daniel <simpledaniel.1818@gmail.com>
2020-02-05 12:31:32 +0300

    react native state

---
+++ src/component/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+    return (
+<Fragment>
+<h2>About the Application</h2>
+<p>This is Github finder for the users</p> 
+</Fragment>
+    )
+}
+
+export default About
